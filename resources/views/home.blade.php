72b103f6fe550733c9d0021792c34d101716a796
M. Fajrul Fatih Abul Ilmi <muhfajrulfatih@gmail.com>
2025-09-10 13:24:34 +0700

    buat branch baru dev

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+</head>
+    <body>
+        <header class="header">
+            <h1>Web Saya</h1>
+            <nav class="nav">
+                <ul class="nav-list">
+                    <li class="nav-item"><a href="#">Home</a></li>
+                    <li class="nav-item"><a href="#">About</a></li>
+                    <li class="nav-item"><a href="#">Services</a></li>
+                    <li class="nav-item"><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        
+        <main>
+            <section></section>
+            <article></article>
+            <aside></aside>
+        </main>
+
+        <footer></footer>
+    </body>
+</html>
