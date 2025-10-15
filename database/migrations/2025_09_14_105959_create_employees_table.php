99c1dbdc5d479d921aeb7ac63e72f0acc7ef24cb
M. Fajrul Fatih Abul Ilmi <muhfajrulfatih@gmail.com>
2025-10-15 13:57:53 +0700

    Penambahan migrasi semua tabel

--- database/migrations/2025_09_14_105959_create_employees_table.php
+++ database/migrations/2025_09_14_105959_create_employees_table.php
@@ -11,22 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-/*
-        Schema::create('departements', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_departemen', 100);
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('positions', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_jabatan', 100);
-            $table->decimal('gaji_pokok', 10, 2);
-            $table->rememberToken();
-            $table->timestamps();
-        });
-*/
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('nama_lengkap', 100);
@@ -35,30 +19,15 @@ return new class extends Migration
             $table->date('tanggal_lahir');
             $table->text('alamat');
             $table->date('tanggal_masuk');
-            // $table->foreignId('departemen_id')->constrained('departements')->onDelete('cascade');
-            // $table->foreignId('jabatan_id')->constrained('positions')->onDelete('cascade');
             $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
             $table->rememberToken();
             $table->timestamps();
         });
-
-/*
-        Schema::create('attendence', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('employee_id')->constrained('employees')->onDelete('cascade');
-            $table->date('tanggal');
-            $table->timestamp('waktu_masuk')->nullable();
-            $table->timestamp('waktu_keluar')->nullable();
-            $table->enum('status_absensi', ['hadir', 'izin', 'sakit', 'alpha'])->default('hadir');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-}
-*/
     }
-/**
- * Reverse the migrations.
-*/
+
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('employees');
---
+++ database/migrations/2025_10_15_041059_create_attendance_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendence', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('karyawan_id');
+            $table->date('tanggal');
+            $table->timestamp('waktu_masuk')->nullable();
+            $table->timestamp('waktu_keluar')->nullable();
+            $table->enum('status_absensi', ['hadir', 'izin', 'sakit', 'alpha'])->default('hadir');
+            $table->rememberToken();
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('karyawan_id')
+                ->references('id')
+                ->on('employees')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance');
+        Schema::table('attendance', function (Blueprint $table) {
+            $table->dropForeign(['karyawan_id']);
+            $table->dropColumn(['karyawan_id']);
+        });
+    }
+};
